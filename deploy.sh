a65d86163ad8bb5011be12ce4aeb61f58c61c3e3
Jlightdevops <pstjlight@gmail.com>
2022-08-28 17:29:16 +0000

    deplyment

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#deploy a python web application
+echo "hello world" = shell scripts
+print ('hello world') = python scripts
+vi cases
+add other tools like aws and git
+.git status
+untracked file -deploy.sh and modified -cases
